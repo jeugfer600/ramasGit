59f7845adef3e73b693928d3b8920ae894620249
= <=>
2024-02-09 09:11:37 +0100

    add division

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public int division( int a, int b){
+        return a/b;
+
     }
 }
